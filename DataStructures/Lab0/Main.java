e6b0ed3976b796e5dea4854e5a39c1813f27ce07
kjblakeley <kevin.blakeley@upr.edu>
2018-02-26 21:43:20 -0400

    Add files via upload

---
+++ DataStructures/Lab0/Main.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter the radius: ");
+		int radius = keyboard.nextInt();
+		
+		double area= Math.PI*radius*radius;
+		double halfArea = area/2;
+		System.out.println("The area of a circle with radius = "+radius+", is: " + area);
+		System.out.println("Half the circle area is: "+ halfArea);
+		
+	}
+
+}
